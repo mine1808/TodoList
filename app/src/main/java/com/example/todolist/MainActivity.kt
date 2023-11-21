2f3f99ebbdeee0a87efaab342cc80512adef4f1b
mine1808 <salwajasmineaaliyah2004@mail.ugm.ac.id>
2023-11-21 17:57:03 +0700

    up

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolist.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TaskItemClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private val taskViewModel: TaskViewModel by viewModels{
+        TaskItemModelFactory((application as TodoApplication).repository)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
+        }
+        setRecyclerView()
+    }
+    private fun setRecyclerView()
+    {
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            binding.todoListRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it, mainActivity)
+            }
+        }
+    }
+    override fun editTaskItem(taskItem: TaskItem)
+    {
+        NewTaskSheet(taskItem).show(supportFragmentManager,"newTaskTag")
+    }
+
+    override fun completeTaskItem(taskItem: TaskItem)
+    {
+        taskViewModel.setCompleted(taskItem)
     }
 }
--- app/src/main/java/com/example/todolist/NewTaskSheet.kt
+++ app/src/main/java/com/example/todolist/NewTaskSheet.kt
@@ -1,59 +1,103 @@
 package com.example.todolist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.app.TimePickerDialog
+import android.os.Build
+import android.text.Editable
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolist.databinding.FragmentNewTaskSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.time.LocalTime
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NewTaskSheet.newInstance] factory method to
- * create an instance of this fragment.
- */
-class NewTaskSheet : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class NewTaskSheet(var taskItem: TaskItem?) : BottomSheetDialogFragment()
+{
+    private lateinit var binding: FragmentNewTaskSheetBinding
+    private lateinit var taskViewModel: TaskViewModel
+    private var dueTime: LocalTime? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val activity = requireActivity()
+
+        if (taskItem != null)
+        {
+            binding.taskTitle.text = "Edit Task"
+            val editable = Editable.Factory.getInstance()
+            binding.name.text = editable.newEditable(taskItem!!.name)
+            binding.desc.text = editable.newEditable(taskItem!!.desc)
+            if(taskItem!!.dueTime() != null)
+            {
+                dueTime = taskItem!!.dueTime()!!
+                updateTimeButtonText()
+            }
+        }
+        else
+        {
+            binding.taskTitle.text = "New Task"
+        }
+
+        taskViewModel = ViewModelProvider(activity).get(TaskViewModel::class.java)
+        binding.saveButton.setOnClickListener {
+            saveAction()
+        }
+        binding.timePickerButton.setOnClickListener {
+            openTimePicker()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_task_sheet, container, false)
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun openTimePicker() {
+        if(dueTime == null)
+            dueTime = LocalTime.now()
+        val listener = TimePickerDialog.OnTimeSetListener{ _, selectedHour, selectedMinute ->
+            dueTime = LocalTime.of(selectedHour, selectedMinute)
+            updateTimeButtonText()
+        }
+        val dialog = TimePickerDialog(activity, listener, dueTime!!.hour, dueTime!!.minute, true)
+        dialog.setTitle("Task Due")
+        dialog.show()
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewTaskSheetFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewTaskSheet().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun updateTimeButtonText() {
+        binding.timePickerButton.text = String.format("%02d:%02d",dueTime!!.hour,dueTime!!.minute)
     }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentNewTaskSheetBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun saveAction()
+    {
+        val name = binding.name.text.toString()
+        val desc = binding.desc.text.toString()
+        val dueTimeString = if(dueTime == null) null else TaskItem.timeFormatter.format(dueTime)
+        if(taskItem == null)
+        {
+            val newTask = TaskItem(name,desc, dueTimeString,null)
+            taskViewModel.addTaskItem(newTask)
+        }
+        else
+        {
+            taskItem!!.name = name
+            taskItem!!.desc = name
+            taskItem!!.dueTimeString = dueTimeString
+
+            taskViewModel.updateTaskItem(taskItem!!)
+        }
+        binding.name.setText("")
+        binding.desc.setText("")
+        dismiss()
+    }
+
 }
